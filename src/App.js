f87a328a7f5ab3fb7f18d7cfe5407bd67ed5af22
santiago Morales <chagoe_97@hotmail.com>
2019-09-13 15:56:39 -0500

    particle.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Particle from "./components/Particle"
+import NavBar from './components/NavBar'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="part">
+        <NavBar />
+        <Particle />
+      </section>
+
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const NavBar = () => (
+
+    <div>
+
+        <ul >
+            <li >
+                <a href="/#"  >Home</a>
+            </li>
+            <li >
+                <a href="/#"  >Features</a>
+            </li>
+            <li >
+                <a href="/#" >Pricing</a>
+            </li>
+            <li >
+                <a href="/#" >Disabled</a>
+            </li>
+        </ul>
+    </div>
+);
+
+export default NavBar;
